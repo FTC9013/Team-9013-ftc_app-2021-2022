b0eae6bb34c73880804d72a635616d3237839bbc
9013Matrix <9013Matrix@gmail.com>
2021-01-12 18:12:21 -0500

    Update AutonomousPrimary.java
    Fixed camera viewing settings - 1/12/2021

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousPrimary.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousPrimary.java
@@ -4,10 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+
+
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
@@ -15,10 +15,6 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
-import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
-import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
-import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
 
 @Autonomous(name = "Auto Primary", group = "Linear Opmode")
 
@@ -341,10 +337,10 @@ public class AutonomousPrimary extends LinearOpMode
         "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
 
     // init with monitor scree
-    // TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+    TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
 
     // init with no monitor screen
-     TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters();
+    // TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters();
 
     // set the minimumConfidence to a higher percentage to be more selective when identifying objects.
 //    tfodParameters.minimumConfidence = 0.45;
